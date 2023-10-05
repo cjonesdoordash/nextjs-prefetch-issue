15d2c5ffba772f754e9d1e2433bc14212ef47714
Cody Jones <cody.jones@doordash.com>
2023-10-05 15:08:01 -0700

    Inner template issue

--- app/templateClientComponent.tsx
+++ app/templateClientComponent.tsx
@@ -4,14 +4,15 @@ import React from 'react'
 
 export const TemplateClientComponent = ({
   children,
+  isInner,
   seconds,
-}: React.PropsWithChildren<{ seconds: number }>) => {
+}: React.PropsWithChildren<{ isInner?: boolean; seconds: number }>) => {
   const [count, setCount] = React.useState(0)
 
   return (
     <>
       <div style={{ padding: '32px', border: '1px solid black' }}>
-        <p>Template Client Component</p>
+        <p>{isInner ? 'Inner' : ''} Template Client Component</p>
         <p>Seconds from server {seconds}</p>
         <p>Counter: {count}</p>
         <button onClick={() => setCount((prev) => prev + 1)}>Increment</button>
